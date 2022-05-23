d3973e80ebf388c714706a8f208f51d7e3b197cf
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-23 10:31:10 -0700

    update

--- Module test/module2-2.c
+++ Module test/module2-2.c
@@ -5,8 +5,9 @@ void updateArray1(int array[]) {
     array[0] = 10;
 }
 
-void upateArray2(int* array[]) {
-    (*array)[0] = 10;
+void updateArray2(int* array) {
+    *array = 6;
+    *(array + 2) = 8;  //pointer arithmetic
 }
 
 int main(){
@@ -22,13 +23,18 @@ int main(){
     printf("%d\n", numbers[1]);
     printf("%d\n", numbers[2]);
 
-    piintf("---------------");
-    updateArray2(&numbers);
+    printf("---------------\n");
+    updateArray2(numbers);
 
     // printf("%p\n", &numbers[0]);
     // printf("%p\n", &numbers[1]);
     // printf("%p\n", &numbers[2]);
     
+    printf("%p\n", numbers);  //it contains the address of the first element
+    printf("%p\n", numbers + 1);
+    printf("%p\n", numbers + 2);
+
+
     printf("%d\n", numbers[0]);
     printf("%d\n", numbers[1]);
     printf("%d\n", numbers[2]);
