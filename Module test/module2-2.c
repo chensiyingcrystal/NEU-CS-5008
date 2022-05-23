4bdf556707b46b8c42c3bc25596dcc89723deb29
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-23 10:21:23 -0700

    Update module2-2.c

--- Module test/module2-2.c
+++ Module test/module2-2.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void updateArray1(int array[]) {
+    array[0] = 10;
+}
+
+void upateArray2(int* array[]) {
+    (*array)[0] = 10;
+}
+
 int main(){
     int numbers[3] = {1, 2, 3};
 
-    print("%p\n", &numbers[0]);
-    print("%p\n", &numbers[1]);
-    print("%p\n", &numbers[2]);
+    updateArray1(numbers);
+
+    // printf("%p\n", &numbers[0]);
+    // printf("%p\n", &numbers[1]);
+    // printf("%p\n", &numbers[2]);
+    
+    printf("%d\n", numbers[0]);
+    printf("%d\n", numbers[1]);
+    printf("%d\n", numbers[2]);
+
+    piintf("---------------");
+    updateArray2(&numbers);
+
+    // printf("%p\n", &numbers[0]);
+    // printf("%p\n", &numbers[1]);
+    // printf("%p\n", &numbers[2]);
+    
+    printf("%d\n", numbers[0]);
+    printf("%d\n", numbers[1]);
+    printf("%d\n", numbers[2]);
+
+    return 0;
 }
