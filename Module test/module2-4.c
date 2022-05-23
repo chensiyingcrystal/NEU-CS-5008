f02eb79e8d1907a8c4e84d5bf4c30a22ce68689f
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-23 14:20:24 -0700

    update

--- Module test/module2-4.c
+++ Module test/module2-4.c
@@ -6,9 +6,31 @@ typedef struct node {
     struct node* next;
 }node_t;
 
+void printNodes(node_t* node) {
+    node_t* itr = node; //create an iterator
+    while (itr != NULL) {
+        print("%d\n", itr->data);
+        itr = itr->next;
+    }
+}
+
 int main() {
     node_t node1;
     node_t node2;
     node_t node3;
+
+    node1.data = 1;
+    node2.data = 2;
+    node3.data = 3;
+
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;
+
+
+    
+
+
+
     return 0;
 }
