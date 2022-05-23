9c7f2c0a82bd33b595293820fea6d9ccb3678687
Siying Chen <chensiyingcrystal@gmail.com>
2022-05-23 13:56:58 -0700

    update

--- Module test/module2-3.c
+++ Module test/module2-3.c
@@ -60,12 +60,43 @@ int push_back(vector_t* vector, int element) {
     if (vector == NULL) return 0;
 
     if (vector->size == vector->capacity) { //when the vector is full, we have to double it
-        resize(vector);
-        return;
+        int resizeSuccess = resize(vector);
+        if (resizeSuccess == 0) return 0;
     }
 
     (vector->data)[vector->size] = element;
     (vector->size)++;
+    return 1;
+}
+
+int insert(vector_t* vector, int pos, int element) {
+    if (vector == NULL || pos < 0) {
+        return 0;
+    }
+
+    if (pos == vector->size) {
+        push_back(vector, element);
+        return 1;
+    }
+
+    if (vector->size == vector->capacity) {
+        int currSize = resize(vector);
+        if (currSize == 0) return 0;
+    }
+    
+    if (vector->data == NULL) return 0;
+
+    for (int i = vector->size; i > pos; i--) {
+        (vector->data)[i] = (vector->data)[i - 1];
+    }
+
+    (vector->data)[pos] = element;
+    vector->size++;
+    return 1;
+}
+
+int push_front(vector_t* vector, int element) {
+    return insert(vector, 0, element);
 }
 
 
---
+++ Module test/module2-4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+}node_t;
+
+int main() {
+    node_t node1;
+    node_t node2;
+    node_t node3;
+    return 0;
+}
